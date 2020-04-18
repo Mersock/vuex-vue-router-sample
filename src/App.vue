eba9dab6cd0b0b29b60b6040bd308c589eff8228
knzphumthawan <knzphumthawan@gmail.com>
2020-04-18 20:40:29 +0700

    initail page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/cats">Cat</router-link> |
+      <router-link to="/dogs">Dog</router-link> |
+      <router-link to="/pets">Pet</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
