bc38b6ca931d2b650a38a4c3ea07ab2167432299
knzphumthawan <knzphumthawan@gmail.com>
2020-04-19 08:58:17 +0700

    push store

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,20 +3,23 @@
     <h1>Cats for Adoption</h1>
     <b-table :items="cats">
       <template v-slot:cell(name)="data">
-        <router-link :to="`/pets/${data.value}`">{{ data.value }}</router-link>
+        <router-link :to="`/pets/${data.index}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 
 export default {
   data () {
-    return {
-      cats
-    }
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
 }
 </script>
