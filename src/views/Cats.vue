a8a5ce8598e7b34c4fb8d50e33e8ca80964e7467
knzphumthawan <knzphumthawan@gmail.com>
2020-04-19 08:41:44 +0700

    cats link

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+    <b-table :items="cats">
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/${data.value}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
